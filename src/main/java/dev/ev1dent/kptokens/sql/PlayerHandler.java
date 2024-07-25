7bcde886e20e6516467265ea2bc52a809d739836
Evidentsinger14 <cdelancey186423@gmail.com>
2024-07-24 20:37:26 -0400

    Attempt to make sure database is connected on player join.

--- src/main/java/dev/ev1dent/kptokens/sql/PlayerHandler.java
+++ src/main/java/dev/ev1dent/kptokens/sql/PlayerHandler.java
@@ -4,12 +4,16 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
+import java.sql.SQLException;
+
 public class PlayerHandler implements Listener {
 
     SQLGetter data = new SQLGetter();
+    MySQL sql = new MySQL();
 
     @EventHandler
-    public void onPlayerJoin(PlayerJoinEvent event) {
+    public void onPlayerJoin(PlayerJoinEvent event) throws SQLException, ClassNotFoundException {
+        if(!sql.isConnected()) sql.connect();
         data.createPlayer(event.getPlayer());
     }
 }
